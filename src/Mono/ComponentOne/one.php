15dcb89285d3d6d2871cd9cfbdab42d6b420cb7d
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2019-03-23 14:23:09 -0500

    re-add component repos'

--- src/Mono/ComponentOne/one.php
+++ src/Mono/ComponentOne/one.php
@@ -2,20 +2,3 @@
 /**
  * Just a test file from Monorepo Component One.
  */
-
-/**
- * Just a test function.
- *
- * Added in 1.1.1.
- */
-function testOne(){
-}
-
-/**
- * A second test function.
- *
- * Added in 1.1.2.
- */
-function testTwo(){
-
-}
--- src/Mono/ComponentTwo/two.php
+++ src/Mono/ComponentTwo/two.php
@@ -2,12 +2,3 @@
 /**
  * Just a test file from Monorepo Component Two.
  */
-
-/**
- * A second test function.
- *
- * Added in 1.1.2.
- */
-function testTwo(){
-
-}
